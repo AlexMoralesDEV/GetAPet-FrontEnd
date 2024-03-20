ca6a987f0f56c3791c34b9f00134e47f45c611ae
unknown <dev.alexmorales@gmail.com>
2024-03-20 18:18:23 -0300

    Correção no Campo Senha

--- src/components/pages/Auth/Login.js
+++ src/components/pages/Auth/Login.js
@@ -25,7 +25,7 @@ function Login(){
             <h1>Login</h1>
             <form onSubmit={ handleSubmit }>
                 <Input name='email' type='email' placeholder='Digite o seu email' text='Email' handleOnChange={ handleChange }/>
-                <Input name='password' type='senha' placeholder='Digite o seu senha' text='Senha' handleOnChange={ handleChange }/>
+                <Input name='password' type='password' placeholder='Digite o seu senha' text='Senha' handleOnChange={ handleChange }/>
                 <input type='submit' value='Entrar' />
             </form>
             <p>Ainda não tem uma conta? <Link to='/register'>Cadastre-se</Link></p>
