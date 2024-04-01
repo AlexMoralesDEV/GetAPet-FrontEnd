54fdfa03f90920ae25729a757bffbf9ce0e832a3
unknown <dev.alexmorales@gmail.com>
2024-04-01 20:42:14 -0300

    Exibindo os Pets na Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react'
+
+import api from '../../utils/api'
+import RoundedImage from '../layouts/RoundedImage'
+
+import styles from './Home.module.css'
+
 function Home(){
+    const [pets, setPets] = useState('')
+
+    useEffect(() => {
+        api.get('/pets/').then((response) => {
+            console.log(response.data.allPets)
+            return setPets(response.data.allPets)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
     return (
-        <section>
-            <h1>Home</h1>
+        <section className={styles.section_flex}>
+            {/* <h1>Pet para Adoção</h1> */}
+            {pets && pets.map((pet) => (
+                <div className={styles.pet_item}>
+                    <RoundedImage width='px75' alt={pet.name} src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}/>
+                    <h1>{pet.name}</h1>
+                    <p><span>Dono: </span>{pet.user.name}</p>
+                </div>
+            ))}
         </section>
     )
 }
